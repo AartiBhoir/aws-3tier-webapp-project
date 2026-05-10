8bf864d7a0c9d62fa1304cf27c155e5f1d629e29
AartiBhoir <bhoiraarti3@gmail.com>
2026-05-10 18:02:51 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+Paste:CREATE TABLE users (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100),
+email VARCHAR(100)
+);
+
+INSERT INTO users(name,email)
+VALUES('Aarti','[aarti@gmail.com](mailto:aarti@gmail.com)');
+
+SELECT * FROM users;
